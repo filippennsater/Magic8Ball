19f49889b0cb963d3cfc3b796eefe16e57b42553
filippennsater <filip.pennsater@hotmail.com>
2023-03-17 12:36:01 +0100

    fixade script

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+function chooseAnswer(){
+var y = Math.floor(Math.random() * 19);
+console.log(y);
+console.log('suckmyballs2');
+printAnswer(y);
+}
+
+function printAnswer(y){
+document.getElementById('answerDiv').innerHTML='Yeebooi he THICC';
+}
