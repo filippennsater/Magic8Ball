3580f3ce4b565bd413dfa0cb2934e5f4ced83b2f
filippennsater <filip.pennsater@hotmail.com>
2023-03-17 20:43:18 +0100

    scrapped trying to read from txt-file into an array in js and just created an array directly instead with the strings. sad.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
+/* ugly solution because it's so annoyingly hard to read from txt-file*/
+var array = ["It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don’t count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"]
 
+function chooseAnswer() {
 
-function chooseAnswer(){
-var y = Math.floor(Math.random() * 21);
-console.log(y);
-console.log('suckmyballs2');
-printAnswer(y);
+    var y = Math.floor(Math.random() * 20);
+    console.log(y);
+    printAnswer(y);
+
 }
 
-function printAnswer(y){
-document.getElementById('outputAnswer').innerHTML='Yeebooi he THICC';
+function printAnswer(y) {
+    document.getElementById('outputAnswer').innerHTML = array[y];
 }
+
+
+function splitAnswers() {
+
+
+}
+
